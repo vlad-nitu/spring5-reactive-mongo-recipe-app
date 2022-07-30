00220a2d3277d3fb21a5c5253f7d9ebc650dec83
Vlad Nitu <V.P.Nitu@student.tudelft.nl>
2022-07-30 18:59:14 +0300

    Added missing imports
    Took 11 seconds

--- src/main/java/guru/springframework/repositories/reactive/CategoryReactiveRepository.java
+++ src/main/java/guru/springframework/repositories/reactive/CategoryReactiveRepository.java
@@ -1,5 +1,6 @@
 package guru.springframework.repositories.reactive;
 
+import guru.springframework.domain.Category;
 import org.springframework.data.mongodb.repository.ReactiveMongoRepository;
 
 public interface CategoryReactiveRepository extends ReactiveMongoRepository<Category, String> {
